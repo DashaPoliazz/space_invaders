4a7b13843a60f9bdd4c208d54505064ac6640075
DashaPoliazz <dasha.poliazzz@gmail.com>
2024-06-26 15:17:01 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const WIDTH = 600;
+const HEIGHT = 800;
+
+window.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+
+  canvas.widht = WIDTH;
+  canvas.height = HEIGHT;
+});
